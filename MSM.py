1cfe2728cc674f9964c1715879cdda2a27ede069
Duoogle <74378827+Duoogle@users.noreply.github.com>
2024-02-01 21:07:44 -0700

    Start
    starting of this journey

---
+++ MSM.py
@@ -0,0 +1,67 @@
+import math
+from pyomo.environ import *
+match_id = input('What\'s the match id? ')
+player1 = input('What is player one name? ')
+player2 = input()
+
+
+elapsed_time = int(input())
+set_no = int(input())
+game_no= int(input())
+point_no = int(input())
+p1_sets = int(input())
+p2_sets = int(input())
+p1_games = int(input())
+p2_games = int(input())
+
+
+p1_score = int(input())
+p2_score = int(input())
+
+
+server = int(input())
+serve_no = int(input())
+point_victor = int(input())
+p1_points_won = int(input())
+p2_points_won = int(input())
+game_victor = int(input())
+set_victor = int(input())
+
+
+
+p1_ace = bool(input())
+p2_ace = bool(input())
+p1_winner = bool(input())
+p2_winner = bool(input())
+
+
+winner_shot_type = input()
+
+
+
+p1_double_fault = bool(input())
+p2_double_fault = bool(input())
+p1_unf_err = bool(input())
+p2_unf_err = bool(input())
+p1_net_pt = bool(input())
+p2_net_pt = bool(input())
+p1_net_pt_won = bool(input())
+p2_net_pt_won = bool(input())
+p1_break_pt = bool(input())
+p2_break_pt = bool(input())
+p2_break_pt_won = bool(input())
+p2_break_pt_won = bool(input())
+p1_break_pt_missed  = bool(input()) 
+p2_break_pt_missed  = bool(input())
+
+
+p1_distance_ran = int(input())
+p2_distance_ran = int(input())
+rally_count = int(input())
+serve_speed = int(input())
+serve_width = input()
+serve_depth = input ()
+return_depth = input ()
+
+
+print(math.pi)
