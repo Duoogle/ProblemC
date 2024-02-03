9763d440e4276b2984161c3d441cb259b189c578
Duoogle <74378827+Duoogle@users.noreply.github.com>
2024-02-02 17:13:13 -0700

    Add files via upload

---
+++ MCM/exceltest.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+dataframe1 = pd.read_excel('Reduced_Data_MCM_2024.xlsx')
+
+#df = pd.DataFrame(
+ #   {
+  #  "A": pd.DataFrame(columns=list("D"))
+   # }
+#)
+
